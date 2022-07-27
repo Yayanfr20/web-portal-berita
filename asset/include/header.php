650c0bb13e88c35f8dd3d99ff6e3d834ec4dc37c
Yayanfr20 <yayanfathurohman20@gmail.com>
2022-07-28 02:30:19 +0700

    menambahkan file

---
+++ asset/include/header.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-dark navbar-expand-lg bg-secondary fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><span class="text-primary">Cihuy</span> <span class="text-danger">news</span></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">News</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Kategori
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Olah raga</a></li>
+            <li><a class="dropdown-item" href="#">Bencana</a></li>
+            <li><a class="dropdown-item" href="#">Game</a></li>
+            <li><a class="dropdown-item" href="#">islam</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+            <a href="" class="nav-link">About</a>
+        </li>
+        <li class="nav-item">
+            <a href="" class="nav-link">Contact</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-primary" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Cihuy news</title>
+	<link rel="stylesheet" href="asset/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+<!-- header -->
+
+<?php
+include 'asset/include/header.php';
+?>
+<!-- end header -->
+<br>
+<br>
+
+<!-- news -->
+<div class="container" style="width:80%;height:300px;margin-left:10%;">
+
+<?php
+include 'asset/include/news.php';
+?>
+</div>
+<!-- end news -->
+
+
+<script src="asset/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="asset/sweetalert2/dist/sweetalert2.all.min.js"></script>
+</body>
+</html>
